33d04496f685a9845549f6fc41327f813ab23ad0
Matthew Ryan <matthew@boxfort.co>
2026-04-05 16:35:16 -0500

    Initial Commit

---
+++ Imposter with Dr. Toast/Imposter_with_Dr__ToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imposter_with_Dr__ToastApp.swift
+//  Imposter with Dr. Toast
+//
+//  Created by Matthew Ryan on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Imposter_with_Dr__ToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
